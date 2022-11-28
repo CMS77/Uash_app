9b6d4a55a21d3324a4421698e1296dbe07bbb18a
marco-nutella <marcoantonio.mp.camargo@gmail.com>
2022-11-28 15:51:02 +0000

    Fixed build issues and began implementing server connections

---
+++ app/src/main/java/com/example/uashapp/java/controllers/JsonReceiverController.java
@@ -0,0 +1,48 @@
+package com.example.uashapp.java.controllers;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JsonReceiverController extends AsyncTask<String, Void, JSONArray> {
+
+    @Override
+    protected JSONArray doInBackground(String... urls) {
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+        String result = "";
+
+        try {
+            url = new URL(urls[0]); // Apenas aceita um URL por chamada
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+
+            int data = reader.read();
+            while(data != -1) {
+                char current = (char) data;
+                result += current;
+                data = reader.read();
+            }
+
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray resultArray = new JSONArray();
+
+            return resultArray;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
